be81de2d7b401d31cebd43e2c836add4a33aeb3f
YuanGenghis <wsyztz@gmail.com>
2020-06-20 12:32:06 -0400

    latest

--- src/main/java/com/example/project/services/UserService.java
+++ src/main/java/com/example/project/services/UserService.java
@@ -1,13 +1,12 @@
 package com.example.project.services;
 
-import com.example.project.models.Product;
 import com.example.project.models.User;
 import com.example.project.repositories.UserRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+
 
 @Service
 public class UserService {
